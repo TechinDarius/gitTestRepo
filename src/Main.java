0bb1bea1a500e2fbbdbf939fda5a2344efca23b5
Darius <mlecka.darius@gmail.com>
2024-03-13 16:31:04 +0200

    refactor: main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("here");
         System.out.println("here is nothing");
     }
 }
